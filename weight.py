3482075ef68515384fdbe01a9ef9cd8cf5147708
unknown <shuujirou.nakano@gmail.com>
2024-07-26 16:58:29 +0900

    重み初期化追加

--- weight.py
+++ weight.py
@@ -16,3 +16,46 @@ class He:
             self.params["b"+str(idx)] = np.zeros(all_layer[idx])
         
         return self.params
+
+class Uni_line:
+    def __init__(self):
+        self.params = {}
+    def weight_initialization(self, inp, layer, out):
+        all_layer = [inp] +  layer + [out]
+
+        for idx in range(1,len(all_layer)):
+            scale = np.sqrt(2.0/all_layer[idx-1])
+            index = scale * np.random.randn(all_layer[idx])
+            self.params["W"+str(idx)] = np.tile(index,(all_layer[idx-1],1))
+            self.params["b"+str(idx)] = np.zeros(all_layer[idx]) 
+        
+        return self.params
+    
+class Uni_row:
+    def __init__(self):
+        self.params = {}
+    def weight_initialization(self, inp, layer, out):
+        all_layer = [inp] + layer + [out]
+
+        for idx in range(1,len(all_layer)):
+            scale = np.sqrt(2.0/all_layer[idx-1])
+            index = scale * np.random.randn(all_layer[idx-1])
+            pre_w = np.tile(index,(all_layer[idx],1))
+            self.params["W"+str(idx)] = pre_w.T
+            self.params["b"+str(idx)] = np.zeros(all_layer[idx])
+    
+        return self.params
+    
+class S_random:
+    def __init__(self):
+        self.params = {}
+    def weight_initialization(self, inp, layer, out):
+        all_layer = [inp] + layer + [out]
+        
+        for idx in range(1,len(all_layer)):
+            scale = np.sqrt(2.0/all_layer[idx-1])
+            self.params["W"+str(idx)] = scale * np.random.rand(all_layer[idx-1],all_layer[idx])
+            self.params["b"+str(idx)] = np.zeros(all_layer[idx])
+
+        
+        return self.params
