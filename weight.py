fdee9b36b014e937441274ba579fc8dbde01f102
unknown <shuujirou.nakano@gmail.com>
2024-08-02 01:02:58 +0900

    RCクラスでレイヤー別にコピー設定できるようにしたLinesクラスはほっといて

--- weight.py
+++ weight.py
@@ -24,10 +24,10 @@ class S_random:
         all_layer = [inp] + layer + [out]
         
         for idx in range(1,len(all_layer)):
-            scale = np.sqrt(9.0/all_layer[idx-1])
+            scale = np.sqrt(100/all_layer[idx-1])
             self.params["W"+str(idx)] = scale * np.random.rand(all_layer[idx-1],all_layer[idx])
             self.params["b"+str(idx)] = np.zeros(all_layer[idx])
-
+            print(self.params["W"+str(idx)].shape)
         
         return self.params
 
@@ -35,21 +35,24 @@ class S_random:
 class RC:
     def __init__(self):
         self.params = {}
-        self.copies = [10,10,10]      #len(copy)が何個の値をコピーするか、配列の値が各値をそれぞれコピーする回数([2,3,4]なら3つランダムな値を選んで、それぞれ2,3,4回ランダムな場所にコピーする)
+        self.copies = [[500,500,500,500],[100,200,200],[200,200,200],[300,300]]
+        #全体の長さがレイヤー数、[100,100,100]はランダムに3つ値をコピーしてそれぞれ100か所ずつ置き換える
     def weight_initialization(self, inp, layer, out):
         all_layer = [inp] + layer + [out]
 
         for idx in range(1,len(all_layer)):
             scale = np.sqrt(2.0/all_layer[idx])
             self.params["W"+str(idx)] = scale * np.random.randn(all_layer[idx-1],all_layer[idx])
-            for jdx in range(1,len(self.copies)):
-                line = np.random.randint(1,all_layer[idx-1]) #コピー元の
-                row = np.random.randint(1,all_layer[idx])    #アドレス指定
-                address = np.random.randint(1,(all_layer[idx-1] * all_layer[idx]),(1,self.copies[jdx]),self.copies[jdx])   #コピー先のアドレス配列生成、調べたら1次元配列の説明しか乗ってなかったので
+            
+            for jdx in range(0,len(self.copies[idx-1])):
+                line = np.random.randint(1,all_layer[idx-1]) #コピー元のアドレス指定
+                row = np.random.randint(1,all_layer[idx]) 
+                address = np.random.randint(1,(all_layer[idx-1] * all_layer[idx]),(self.copies[idx-1][jdx])) 
+                #コピー先のアドレス配列生成、調べたら2次元配列も1次元配列でコピー先指定してたので1次元にしてる
                 np.put(self.params["W"+str(idx)],address,self.params["W"+str(idx)][line][row])
 
             self.params["b"+str(idx)] = np.zeros(all_layer[idx])
-        print(self.params)
+            print(self.params["W"+str(idx)].shape)
         return self.params
     
 
@@ -63,9 +66,18 @@ class Uni_line:
             scale = np.sqrt(2.0/all_layer[idx-1])
             index = scale * np.random.randn(all_layer[idx])
             self.params["W"+str(idx)] = np.tile(index,(all_layer[idx-1],1))
-            self.params["b"+str(idx)] = np.zeros(all_layer[idx]) 
+            self.params["b"+str(idx)] = np.zeros(all_layer[idx])
+            print(self.params["W"+str(idx)].shape) 
         
         return self.params
+    
+class Lines:
+    def __init__(self):
+        self.params = {}
+    def weight_initialization(self, inp, layer, out):
+        all_layer = [inp] + layer + [out]
+
+
     
 class Uni_row:
     def __init__(self):
