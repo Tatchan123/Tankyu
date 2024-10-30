819e2a74254fc6a4ab812327a89ff34e36c0b879
Tatchan123 <166621150+Tatchan123@users.noreply.github.com>
2024-10-31 07:53:52 +0900

    Update jikken2.py

--- jikken2.py
+++ jikken2.py
@@ -22,35 +22,48 @@ data_n = 2000
 (x_train, t_train),(x_test,t_test) = load_mnist(normalize=True)
 data = {"x_train":x_train[:data_n], "t_train":t_train[:data_n], "x_test":x_test[:data_n], "t_test":t_test[:data_n]}
 
-d0_r = d10_r = d15_r = d20_r = d25_r = d30_r = d40_r = d50_r = []
-result = [d10_r,d15_r,d20_r,d25_r,d30_r,d40_r,d50_r]
+result = {}
 layer_size = [15,30,50,100]
 delete_list = [10,15,20,25,30,40,50]
 t = 10
 
 
+
 for layer_n in [1,2,3]:
     for layer_size in [15,30,50,100]:
-        
         layer = np.full(layer_n,layer_size)
         layer = layer.tolist()
-        
-        d0 = Trainer(step=[200], layer=layer, weightinit=He, optimizer="sgd", data=data, batch_size=100, lr=0.04, check=50)
-        d0_r.append(d0.fit())
-        params = d0.params
-        rmwlayer = list(range(2,2+layer_n))
-        
-        
-        for delete_n in range(7):
-            rmw_n = round(layer_size*delete_n/100)
-            for i in range(t):
+        for n in range(t):
+            d0 = Trainer(step=[200], layer=layer, weightinit=He, optimizer="sgd", data=data, batch_size=100, lr=0.04, check=50)
+            if str(layer_size)+"_"+str(layer_n)+"_"+str(0) in result:
+                result[str(layer_size)+"_"+str(layer_n)+"_"+str(0)].append(d0.fit())
+            else:    result[str(layer_size)+"_"+str(layer_n)+"_"+str(0)] = [d0.fit()]
+            params = d0.params
+            rmwlayer = list(range(2,2+layer_n))
+            for i in range(len(delete_list)):
+                rmw_n = round(layer_size*delete_list[i]/100)
                 pa = copy.deepcopy(params)
-                dn = Trainer(step=["count_rmw"], layer=[20], weightinit=He, optimizer="sgd", data=data, batch_size=100, lr=0.04, 
-                            check=50,epsilon=[1e-6,1e-2,1e-2,1.2e-2], complement=True, rmw_layer=[rmwlayer],rmw_n=rmw_n)
-                result[delete_n].append(dn.fit())
-                
+                dn = Trainer(step=["count_rmw"], layer=layer, weightinit=pa, optimizer="sgd", data=data, batch_size=100, lr=0.04, 
+                            check=50,epsilon=[1e-6,1e-2,1e-2,1.2e-2], complement=True, rmw_layer=rmwlayer,rmw_n=rmw_n)
+                if str(layer_size)+"_"+str(layer_n)+"_"+str(i) in result:
+                    result[str(layer_size)+"_"+str(layer_n)+"_"+str(i)].append(dn.fit())
+                else:   result[str(layer_size)+"_"+str(layer_n)+"_"+str(i)] = [dn.fit()]
+    print(layer_n,layer_size)
 wb = openpyxl.load_workbook('100result.xlsx')
 sheet = wb['Sheet1']
+r = 6
+c = 6
+for i in range(8):
+    for j in [1,2,3]:
+        for k in [15,30,50,100]:
+            sheet.cell(row=r, column=c).value = str(i)+str(j)+str(k)
+            for result in result[str(k)+"_"+str(j)+"_"+str(i)]:
+                c += 1
+                sheet.cell(row=r, column=c).value = result[0]
+            r += 1
+            c = 6
+            
+sheet.cell(row=6, column=i+3).value
 
 
 
