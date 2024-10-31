78e7e32f51fb09bdf0f569a7d9a68ed97a1d5360
Tatchan123 <166621150+Tatchan123@users.noreply.github.com>
2024-10-31 16:06:02 +0900

    わああ

--- a.py
+++ a.py
@@ -1,7 +1,9 @@
 import matplotlib.pyplot as pyp
 import numpy as np
 import openpyxl
-wb = openpyxl.load_workbook('result.xlsx')
+import os
+file = os.path.abspath("C:\\Users\\Owner\\Documents\\Tankyu_Shujiro\\Local\\Tankyu\\result.xlsx")
+wb = openpyxl.load_workbook(file)
 sheet = wb['Sheet1']
 sheet.cell(row=5, column=1).value = "aaaaa"
 wb.save('./Excelサンプル.xlsx')
--- jikken2.py
+++ jikken2.py
@@ -10,6 +10,7 @@ import openpyxl
 import copy
 from model1 import *
 from trainer import *
+import os
 
 """
     self, step, layer, weightinit, optimizer, data, batch_size, lr, check, (decreace1, decreace2, epsilon, complement, rmw_layer, delete_n)
@@ -23,14 +24,20 @@ data_n = 2000
 data = {"x_train":x_train[:data_n], "t_train":t_train[:data_n], "x_test":x_test[:data_n], "t_test":t_test[:data_n]}
 
 result = {}
-layer_size = [15,30,50,100]
+layersize = [15,30,50,100]
 delete_list = [10,15,20,25,30,40,50]
+layern = [1,2,3]
 t = 10
 
 
+# layersize = [15]
+# delete_list = [10]
+# t = 1
 
-for layer_n in [1,2,3]:
-    for layer_size in [15,30,50,100]:
+
+
+for layer_n in layern:
+    for layer_size in layersize:
         layer = np.full(layer_n,layer_size)
         layer = layer.tolist()
         for n in range(t):
@@ -41,29 +48,32 @@ for layer_n in [1,2,3]:
             params = d0.params
             rmwlayer = list(range(2,2+layer_n))
             for i in range(len(delete_list)):
+                print(i)
                 rmw_n = round(layer_size*delete_list[i]/100)
                 pa = copy.deepcopy(params)
                 dn = Trainer(step=["count_rmw"], layer=layer, weightinit=pa, optimizer="sgd", data=data, batch_size=100, lr=0.04, 
                             check=50,epsilon=[1e-6,1e-2,1e-2,1.2e-2], complement=True, rmw_layer=rmwlayer,rmw_n=rmw_n)
-                if str(layer_size)+"_"+str(layer_n)+"_"+str(i) in result:
+                if str(layer_size)+"_"+str(layer_n)+"_"+str(i+1) in result:
                     result[str(layer_size)+"_"+str(layer_n)+"_"+str(i)].append(dn.fit())
-                else:   result[str(layer_size)+"_"+str(layer_n)+"_"+str(i)] = [dn.fit()]
+                else:   result[str(layer_size)+"_"+str(layer_n)+"_"+str(i+1)] = [dn.fit()]
     print(layer_n,layer_size)
-wb = openpyxl.load_workbook('100result.xlsx')
+
+print(result)
+file = os.path.abspath("C:\\Users\\Owner\\Documents\\Tankyu_Shujiro\\Local\\Tankyu\\jikken2.xlsx")
+wb = openpyxl.load_workbook(file)
 sheet = wb['Sheet1']
-r = 6
-c = 6
-for i in range(8):
-    for j in [1,2,3]:
-        for k in [15,30,50,100]:
-            sheet.cell(row=r, column=c).value = str(i)+str(j)+str(k)
-            for result in result[str(k)+"_"+str(j)+"_"+str(i)]:
-                c += 1
-                sheet.cell(row=r, column=c).value = result[0]
-            r += 1
-            c = 6
+ro = 6
+co = 6
+for i in range(len(delete_list)+1):
+    for j in layern:
+        for k in layersize:
+            sheet.cell(row=ro, column=co).value = str(k)+str(j)+str(i)
+            for r in result[str(k)+"_"+str(j)+"_"+str(i)]:
+                co += 1
+                sheet.cell(row=int(ro), column=int(co)).value = r[0]
+            ro += 1
+            co = 6
             
-sheet.cell(row=6, column=i+3).value
 
 
 
